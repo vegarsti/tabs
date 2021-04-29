6bd6f76da093cb74e2bc5477a4800e8a71d9e2ee
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-04-29 10:53:21 +0200

    Read file and populate continuously, until interrupt

--- cmd/d/main.go
+++ cmd/d/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/vegarsti/tabs"
 	"github.com/vegarsti/tabs/firefox"
@@ -20,37 +23,56 @@ func main() {
 		fmt.Fprintln(os.Stderr, "set environment variable TABS_DB_PATH")
 		os.Exit(1)
 	}
-	file, err := os.Open(firefoxPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "couldn't open file '%s': %v\n", firefoxPath, err)
-		os.Exit(1)
+	interruptChannel := make(chan os.Signal, 1)
+	defer close(interruptChannel)
+	signal.Notify(interruptChannel, os.Interrupt, syscall.SIGTERM)
+	ticks := make(chan struct{}, 1)
+	defer close(ticks)
+	go sendTick(ticks, 500*time.Millisecond)
+
+	for {
+		select {
+		case <-interruptChannel:
+			fmt.Println()
+			return
+		case <-ticks:
+			if err := run(firefoxPath, dbPath); err != nil {
+				close(interruptChannel)
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
 	}
+}
 
+func run(firefoxPath string, dbPath string) error {
 	var t tabs.TabService
-
-	t, err = firefox.NewTabService(file)
+	t, err := firefox.NewTabService(firefoxPath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "couldn't create firefox tab service: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("couldn't create firefox tab service: %w", err)
 	}
 	tabs, err := t.ReadTabs()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "read: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("read: %w", err)
 	}
 	t.Close()
 	t, err = sqlite.NewTabService(dbPath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "couldn't create sqlite tab service: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("couldn't create sqlite tab service: %w", err)
 	}
 	defer t.Close()
 	if err := t.WriteTabs(tabs); err != nil {
-		fmt.Fprintf(os.Stderr, "sqlite write tabs: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("sqlite write tabs: %w", err)
 	}
 	if _, err := t.ReadTabs(); err != nil {
-		fmt.Fprintf(os.Stderr, "sqlite read tabs: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("sqlite read tabs: %w", err)
+	}
+	return nil
+}
+
+func sendTick(ch chan<- struct{}, rate time.Duration) {
+	ch <- struct{}{}
+	for range time.Tick(rate) {
+		ch <- struct{}{}
 	}
 }
