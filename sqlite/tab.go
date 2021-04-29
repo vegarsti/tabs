90fe07a11e868141646c1318f80a709ca6015d4e
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-04-29 11:03:15 +0200

    Add title to database and insert one by one

--- sqlite/tab.go
+++ sqlite/tab.go
@@ -3,7 +3,6 @@ package sqlite
 import (
 	"database/sql"
 	"fmt"
-	"strings"
 
 	_ "github.com/mattn/go-sqlite3" // sqlite driver
 	"github.com/vegarsti/tabs"
@@ -19,7 +18,10 @@ func NewTabService(file string) (*TabService, error) {
 	if err != nil {
 		return nil, fmt.Errorf("sqlite open '%s': %w", file, err)
 	}
-	migration := "DROP TABLE IF EXISTS firefox; CREATE TABLE firefox (url text not null, at integer not null);"
+	migration := `
+		DROP TABLE IF EXISTS firefox;
+		CREATE TABLE firefox (url text not null, title text not null, at integer not null);
+	`
 	if _, err := db.Exec(migration); err != nil {
 		return nil, fmt.Errorf("migration: %w", err)
 	}
@@ -34,24 +36,19 @@ func (s *TabService) ReadTabs() ([]tabs.Tab, error) {
 }
 
 func (s *TabService) WriteTabs(tt []tabs.Tab) error {
-	if len(tt) == 0 {
-		return nil
-	}
-	vv := make([]string, len(tt))
-	for i, t := range tt {
-		vv[i] = fmt.Sprintf("('%s', %d)", t.URL, t.LastAccessed)
-	}
-	values := strings.Join(vv, ", ")
-	result, err := s.db.Exec("INSERT INTO firefox (url, at) VALUES " + values + ";")
-	if err != nil {
-		return fmt.Errorf("insert tabs: %w", err)
-	}
-	n, err := result.RowsAffected()
-	if err != nil {
-		return fmt.Errorf("rows affected: %w", err)
-	}
-	if int(n) != len(tt) {
-		return fmt.Errorf("wrote %d rows, but expected to write %d", n, len(tt))
+	for _, t := range tt {
+		insert := "INSERT INTO firefox (url, title, at) VALUES ($1, $2, $3);"
+		result, err := s.db.Exec(insert, t.URL, t.Title, t.LastAccessed)
+		if err != nil {
+			return fmt.Errorf("insert tabs: %w", err)
+		}
+		n, err := result.RowsAffected()
+		if err != nil {
+			return fmt.Errorf("rows affected: %w", err)
+		}
+		if int(n) != 1 {
+			return fmt.Errorf("expected to write a row")
+		}
 	}
 	return nil
 }
